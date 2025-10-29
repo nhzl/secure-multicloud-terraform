6b851ea647854cd16972ca89add692c4cb5370e3
unknown <nicholas@hezel.me>
2025-10-29 00:04:15 -0500

    add bucket_prefix

--- azure/versions.tf
+++ azure/versions.tf
@@ -2,13 +2,13 @@ terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
   }
 }
 
-provider "aws" {
-  region = var.aws_region
+provider "azurerm" {
+  features {}
 }
