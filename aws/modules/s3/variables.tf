2a91fc7af9ff929e3447e2949dfbd19b48f7e1aa
unknown <nicholas@hezel.me>
2025-10-29 15:07:42 -0500

    fix: correct module outputs v2

--- aws/modules/s3/variables.tf
+++ aws/modules/s3/variables.tf
@@ -1,28 +1,28 @@
 variable "bucket_prefix" {
-  type = string                             ### e.g. "secure-multicloud"
+  type = string ### e.g. "secure-multicloud"
 }
 
 variable "force_destroy" {
   type    = bool
-  default = false                            ### optional safety off by default
+  default = false ### optional safety off by default
 }
 
 variable "versioning_enabled" {
   type    = bool
-  default = true                             ### best practice: on
+  default = true ### best practice: on
 }
 
 variable "sse_algorithm" {
   type    = string
-  default = "AES256"                         ### or "aws:kms"
+  default = "AES256" ### or "aws:kms"
 }
 
 variable "kms_key_id" {
-  type      = string
-  default   = null                           ### optional KMS support
+  type    = string
+  default = null ### optional KMS support
 }
 
 variable "tags" {
   type    = map(string)
-  default = {}                               ### user-provided tags
+  default = {} ### user-provided tags
 }
